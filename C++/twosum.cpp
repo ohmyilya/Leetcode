84e2501a5c98331c37d3df626d456e28970e952f
ilya <user@Users-MacBook-Pro.local>
2023-09-15 17:23:23 -0700

    Added C++/ directory

---
+++ C++/twosum.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> numToIndex;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (numToIndex.find(complement) != numToIndex.end()) {
+                return {numToIndex[complement], i};
+            }
+            numToIndex[nums[i]] = i;
+        }
+        return {}; // Return an empty vector if no solution is found.
+    }
+};
